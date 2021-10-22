f14baad9e4d1cfc5f884c2dfb954e6d0db7eb723
hyyryaapolainen <a.hyyrylainen79@gmail.com>
2021-10-22 12:45:30 +0300

    part2 tehtävät valmis

--- osa2/Tehtävät/puhelinluettelo/App.js
+++ osa2/Tehtävät/puhelinluettelo/App.js
@@ -14,7 +14,7 @@ const Phonebook = ({list, filter, onDelete}) => (
     .toLowerCase()
     .startsWith(filter.toLowerCase()))
     .map((person, i) =>
-      <Number key={i} name={person.name} number={person.number} onDelete={(id) => onDelete(id)}/>)}
+      <Number key={i} name={person.name} number={person.number} onDelete={(id) => onDelete(person.id)}/>)}
   </div>
 )
 
@@ -34,48 +34,132 @@ const Field = ({onChange, fieldName, value}) => {
       </form>
     )
 }
+const Notification = ({ message }) => {
+  const errorStyle = {
+    color: 'red',
+    backGround: 'pink',
+    fontSize: '20px',
+    borderStyle: 'solid',
+    borderRadius: '5px',
+    padding: '10px',
+    marginBottom: '10px'
+  }
+  const successStyle = {
+    color: 'green',
+    backGround: 'lightgreen',
+    fontSize: '20px',
+    borderStyle: 'solid',
+    borderRadius: '5px',
+    padding: '10px',
+    marginBottom: '10px'
+  }
+  
+  if (message === null) {
+    return null
+  }
+  if (message.id === 0)
+  {
+    return (
+      <div style={errorStyle}>
+        {message.c}
+      </div>
+    )
+  }
+  else{
+    return (
+      <div style={successStyle}>
+        {message.c}
+      </div>
+    )
+  }
+}
 const App = () => {
 
   const addNewNumber = (event) =>
   {
     event.preventDefault()
     const newEntry = {name: newName, number: newNumber}
-    if (persons.filter(a => a.name === newName).length > 0)
+    const personCheck = [...persons].filter(a => a.name === newName)
+    if (personCheck.length > 0)
     {
-      alert(`${newName} is already added to phonebook`)
+      if (personCheck[0].number !== newNumber)
+      {
+        if(window.confirm(`${newName} is already added to phonebook, replace old number with new one?`))
+        {
+          const id = personCheck[0].id
+          puhelinService
+          .getAll
+          .then(response=> {
+           setPersons(response.data) 
+          })
+          .update(id, newEntry)
+          .then(returnedNmb => {
+            setPersons(persons.map(p => p.id !== returnedNmb.data.id ? p : returnedNmb.data))
+            setNoti(`${newEntry.number}updated number!`, 1)
+          })
+          .catch(error => {setNoti('updating number failed', 0)})
+          setNewName('')
+          setNewNumber('')
+        }
+        }
+        else {
+          alert(`${newName} is already added to phonebook`)
+      }
     }
     else{
     puhelinService
       .create(newEntry)
       .then(response => {
         setPersons(persons.concat(response.data))
+        setNoti(`${newEntry.name}Added number!`, 1)
       })
+      .catch(error => {setNoti(`${error}Adding number failed`, 0)})
     setNewName('')
     setNewNumber('')
     }
   }
   const deleteNumber = (id) =>
-  { 
-    puhelinService
-    .remove(id)
-    .then(response => {
-      setPersons(persons.concat(response.data))
-    })
-  setNewName('')
-  setNewNumber('')
+  {
+    const personToDelete = [...persons].find(a=> a.id === id).name
+    if(window.confirm(`Do you really want to delete ${personToDelete}`))
+    {
+      puhelinService
+      .remove(id)
+      .then(response => {
+        setNoti(`Deleted ${personToDelete}'s number!`, 1)
+        setPersons(persons.filter(a => a.id !== id))
+      })
+      .catch(error=>{setNoti('Deleting number failed', 0)})
+      puhelinService
+      .getAll()
+      .then(response => {
+        setPersons(response.data)
+      })
+    }
   }
 
   const [ persons, setPersons] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filter, setFilter ] = useState('')
+  const [notiMessage, setNotiMessage] = useState(null)
+  const setNoti = (content, id) => {
+    setNotiMessage({c: content,id: id})
+    setTimeout(() => {
+      setNotiMessage(null)
+    }, 5000)
+  }
+
   useEffect(() => {
-    puhelinService
+    const refreshPage = () => {
+      puhelinService
       .getAll()
       .then(response => {
         setPersons(response.data)
       })
-  }, [])
+    }
+    refreshPage()
+    },[])
   const handleFieldChange = (event) => {
     setNewName(event.target.value)
   }
@@ -100,6 +184,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={notiMessage} />
       <Field onChange={(event) => handleFilterFieldChange(event)}
       fieldName="Filter"
       value={filter}/>
@@ -111,7 +196,6 @@ const App = () => {
 }
 
 export default App
-
 
 /*
     "scripts": {
