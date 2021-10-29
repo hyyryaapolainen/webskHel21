38606e96e5c9f8e3bbf82925e7f3dd8e1fa11755
hyyryaapolainen <a.hyyrylainen79@gmail.com>
2021-10-29 09:13:53 +0300

    3.12

--- osa3/Tehtävät/phonebook/puhBack/mongo.js
+++ osa3/Tehtävät/phonebook/puhBack/mongo.js
@@ -1,15 +1,12 @@
 
 const mongoose = require('mongoose')
 
-if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
-}
-
 const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
 
 const url = `mongodb+srv://fullstack:${password}@webskapp.i3wc0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-console.log(process.argv[2])
+
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
@@ -20,11 +17,23 @@ const personSchema = new mongoose.Schema({
 const Person = mongoose.model('Person', personSchema)
 
 const person = new Person({
-  name: 'Mongoose Hellas',
-  number: '04405553214'
+  name: name,
+  number: number
 })
 
-person.save().then(response => {
-  console.log('note saved!')
-  mongoose.connection.close()
-})
+if (process.argv.length<4) {
+
+  Person.find({}).then(result => {
+    console.log("phonebook:")
+    result.forEach((person) => {
+      console.log(person)
+    })
+    mongoose.connection.close()
+  })
+}
+  if (process.argv.length === 5) {
+    person.save().then(response => {
+      console.log(`added ${name} number ${number} to phonebook`)
+      mongoose.connection.close()
+    })
+  }
